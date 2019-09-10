4667ce96d229454bbb7a7b00e6744c47ec16d1c3
Tự Đỗ <dovantuit@gmail.com>
2019-09-10 22:46:59 +0700

    thêm wifi nha

--- src/services/config/index.js
+++ src/services/config/index.js
@@ -1,5 +1,6 @@
 
-const hostApi = `http://192.168.0.101:3000`;
+// const hostApi = `http://192.168.0.101:3000`;
+const hostApi = `http://192.168.0.106:3000`; // ở nhà
 
 // const hostApi = `http://10.0.5.180:3000`;
 
